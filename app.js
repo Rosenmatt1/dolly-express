3abf99f3e1ed72f645c894859f51f44fbcbf8381
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-01-17 13:01:03 -0700

    created two different 404 error andlers for routes

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const app = express() 
 const port = 3003
-
+const cors = require('cors')
 const data = require("./data.json")
 
+app.use(cors())
+
 app.use(express.static('./public'))
+
 
 app.get('/data', (req, res, next) => {
   res.status(200).send({
@@ -14,7 +17,17 @@ app.get('/data', (req, res, next) => {
 })
 
 app.get('/:tag', (req,res, next) => {
-  res.status(200).send(req.params.tag)
-} )
+  const tag = req.params.tag
+  if (!data.tags.includes(tag)) {
+    res.status(404).send('SOrry, that tag does not exist')
+  } else {
+    const matching = data.songs.filter(song => song.tags.includes(tag))
+    res.status(200).send(matching)
+  }
+})
+
+app.use((req, res, next) => {
+  res.status(404).send("This route does not exist")
+})
 
 app.listen(port, () => console.log("party on port 3003!"))
